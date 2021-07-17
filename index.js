6947d9fab36cb239d862970b17c4dbee2920df41
keithkirui91 <keith.kirui33@gmail.com>
2021-07-17 12:47:45 +0300

    .js and  form update .html
    add he function for date and calculating day
    changing the form format to one Date picker.

--- index.js
+++ index.js
@@ -1,89 +1,68 @@
 
+    
     function dayOfTheWeek(){
 
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-    var gender= document.getElementById("gender").value;
-    var result = document.getElementById("result").value;
-
-    var c = year.slice(0, 2);
+        var date = new Date(document.getElementById("date").value);
+        var gender= document.getElementById("gender").value;
+        
+        var s = date.getDay();
     
-    var y = year.slice(2);
-    
-    if(c === '15' || c==='19' || c==='23')
-    {
-        weekDayNo = (3 + (y/12) + (y%12) + ((y%12)/(y/12)));
-    }
-    if(c === '16' || c==='20' || c==='24')
-    {
-        weekDayNo = (2 + (y/12) + (y%12) + ((y%12)/(y/12)));
-    }
-    if(c === '17' || c==='21' || c==='25')
-    {
-        weekDayNo = (0 + (y/12) + (y%12) + ((y%12)/(y/12)));
-    }
-    if(c === '18' || c==='22' || c==='26')
-    {
-        weekDayNo = (5 + (y/12) + (y%12) + ((y%12)/(y/12)));
-    }
 
-    alert("weekday number is "+weekDayNo)
-
-    if( weekDayNo >6)
-    {
-        var finalResult = (weekDayNo%7);
-    }
-    else{
-        var finalResult = (weekDayNo);
-    }
-
-
-    alert("Weekday is" +finalResult);
-
-    if(finalResult === 0 || gender === male);{
+    if(s === 0 && gender === 'M'){
         alert("YOUR NAME IS KWASI");
+        alert("SUNDAY");
     }
-    if(finalResult === 0 || gender === female);{
+    if(s === 0 && gender === 'F'){
         alert("YOUR NAME IS AKOSUA");
+        alert("SUNDAY");
     }
-    if(finalResult === 1 || gender === male);{
+    if(s === 1 && gender === 'M'){
         alert("YOUR NAME IS KWADWO");
+        alert("MONDAY");
     }
-    if(finalResult === 1 || gender === female);{
+    if(s === 1 && gender === 'F'){
         alert("YOUR NAME IS ADOWA");
+        alert("MONDAY");
     }
-    if(finalResult === 2 || gender === male);{
+    if(s === 2 && gender ==='M'){
         alert("YOUR NAME IS KWABENA");
+        alert("TUESDAY");
     }
-    if(finalResult === 2 || gender === female);{
+    if(s === 2 && gender === 'F'){
         alert("YOUR NAME IS ABENAA");
+        alert("TUESDAY");
     }
-    if(finalResult === 3 || gender === male);{
+    if(s === 3 && gender === 'M'){
         alert("YOUR NAME IS KWAKU");
+        alert("WEDNESDAY");
     }
-    if(finalResult === 3 || gender === female);{
+    if(s === 3 && gender === 'F'){
         alert("YOUR NAME IS AKUA");
+        alert("WEDNESDAY");
     }
-    if(finalResult === 4 || gender === male);{
+    if(s === 4 && gender === 'M'){
         alert("YOUR NAME IS YAW");
+        alert("THURSDAY");
     }
-    if(finalResult === 4 || gender === female);{
+    if(s === 4 && gender === 'F'){
         alert("YOUR NAME IS YAA");
+        alert("THURSDAY");
     }
-    if(finalResult === 5 || gender === male);{
+    if(s === 5 && gender ===  'M'){
         alert("YOUR NAME IS KOFI");
+        alert("FRIDAY");
     }
-    if(finalResult === 5 || gender === female);{
+    if(s === 5 && gender === 'F'){
         alert("YOUR NAME IS AFUA");
+        alert("FRIDAY");
     }
-    if(finalResult === 6 || gender === male);{
+    if(s === 6 && gender === 'M'){
         alert("YOUR NAME IS KWAME");
+        alert("SATURDAY");
     }
-    if(finalResult === 6 || gender === female);{
+    if(s === 6 && gender === 'F'){
         alert("YOUR NAME IS AMA");
+        alert("SATURDAY");
     }
-
-    
 
 };
