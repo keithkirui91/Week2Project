082225c3a3ab64f824acc755fb11b13ff12ea8eb
keithkirui91 <keith.kirui33@gmail.com>
2021-07-17 18:46:21 +0300

    Update index.html
    switching data entry for gender to text. Radio was not working as it should.

--- index.js
+++ index.js
@@ -5,64 +5,64 @@
         
 
         var date = new Date(document.getElementById("date").value);
-        const gender= document.getElementById("gender").value;
+        var gender= document.getElementById("gender").value;
         
         const s = date.getDay();
     
 
-    if(s === 0 && gender === 'M'){
+    if(s === 0 && gender === "m"){
         document.write("YOUR NAME IS KWASI  ");
         document.write(" You were born on a SUNDAY");
     }
-    if(s === 0 && gender === 'F'){
+    if(s === 0 && gender === "f"){
         document.write("YOUR NAME IS AKOSUA  ");
         document.write(" You were born on a SUNDAY");
     }
-    if(s === 1 && gender === 'M'){
+    if(s === 1 && gender === "m"){
         document.write("YOUR NAME IS KWADWO  ");
         document.write(" You were born on a MONDAY");
     }
-    if(s === 1 && gender === 'F'){
+    if(s === 1 && gender === "f"){
         document.write("YOUR NAME IS ADOWA  ");
         document.write(" You were born on a MONDAY");
     }
-    if(s === 2 && gender ==='M'){
+    if(s === 2 && gender ==="m"){
         document.write("YOUR NAME IS KWABENA  ");
         document.write(" You were born on a TUESDAY");
     }
-    if(s === 2 && gender === 'F'){
+    if(s === 2 && gender === "f"){
         document.write("YOUR NAME IS ABENAA  ");
         document.write(" You were born on a TUESDAY");
     }
-    if(s === 3 && gender === 'M'){
+    if(s === 3 && gender === "m"){
         document.write("YOUR NAME IS KWAKU  ");
         document.write(" You were born on a WEDNESDAY");
     }
-    if(s === 3 && gender === 'F'){
+    if(s === 3 && gender === "f"){
         document.write("YOUR NAME IS AKUA  ");
         document.write(" You were born on a WEDNESDAY");
     }
-    if(s === 4 && gender === 'M'){
+    if(s === 4 && gender === "m"){
         document.write("YOUR NAME IS YAW  ");
         document.write(" You were born on a THURSDAY");
     }
-    if(s === 4 && gender === 'F'){
+    if(s === 4 && gender === "f"){
         document.write("YOUR NAME IS YAA  ");
         document.write(" You were born on a THURSDAY");
     }
-    if(s === 5 && gender ===  'M'){
+    if(s === 5 && gender ===  "m"){
         document.write("YOUR NAME IS KOFI  ");
         document.write(" You were born on a FRIDAY");
     }
-    if(s === 5 && gender === 'F'){
+    if(s === 5 && gender === "f"){
         document.write("YOUR NAME IS AFUA  ");
         document.write(" You were born on a FRIDAY");
     }
-    if(s === 6 && gender === 'M'){
+    if(s === 6 && gender === "m"){
         document.write("YOUR NAME IS KWAME  ");
         document.write(" You were born on a SATURDAY");
     }
-    if(s === 6 && gender === 'F'){
+    if(s === 6 && gender === "f"){
         document.write("YOUR NAME IS AMA  ");
         document.write(" You were born on a SATURDAY");
     }
